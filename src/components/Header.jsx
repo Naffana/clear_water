55087ce22c4d3f57ae9ea0ffb0f40c3b3d2b20dc
Naffana <Naffanadomovoy@gmail.com>
2025-08-02 20:51:17 +0300

    body_dev

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,11 +21,12 @@ const Header = () => {
         </div>
         </div>
         <div className='Call'>
+          <div className="call_button">
           <p>Позвонить</p>
           <svg width="20" height="20" viewBox="0 0 24.375 24.375" fill="none" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink">
 	          <path id="矢量 13" d="M8.12 0L8.12 3.25L18.83 3.25L0 22.08L2.29 24.37L21.12 5.54L21.12 16.25L24.37 16.25L24.37 0L8.12 0Z" fill="#444444" fill-opacity="1.000000" fill-rule="evenodd"/>
           </svg>
-
+         </div>
         </div>
     </div>
   )
