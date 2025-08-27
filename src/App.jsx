200f4da2f3bfe0e9418679ab79e3daa46a9538e6
Naffana <Naffanadomovoy@gmail.com>
2025-08-28 00:46:03 +0300

    V1.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import { HelmetProvider } from 'react-helmet-async';
 
 function App() {
   return (
-    <React.StrictMode>
-    <HelmetProvider>
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
-    </HelmetProvider>
-    </React.StrictMode>
   );
 }
 
