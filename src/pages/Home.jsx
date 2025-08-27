7db660220d82772b85b0889daf8b1cffca229a72
Naffana <Naffanadomovoy@gmail.com>
2025-08-27 22:07:59 +0300

    V1.1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import Installation from '../components/module/Installation'
 import Repair from '../components/module/Repair'
 import Service from '../components/module/Service'
 import "../styles/Home.css";
+import { Helmet } from 'react-helmet-async';
 
 const Home = () => {
   const [isModalContactOpen, setModalContactOpen] = useState(false);
@@ -16,6 +17,44 @@ const Home = () => {
   const [isModalServiceOpen, setIsModalServiceOpen] = useState(false);
   return (
     <>
+
+    <Helmet>
+        <title>Clear water | Чистые воды - сервис по ремонту, очистке и установка колодцев</title>
+        <meta name="description" content="Копка и установка новых колодцев/септиков. Чистка, обслуживание и ремонт Ваших старых колодцев/септиков. Витебск и близлежащие районы." />
+        <meta name="keywords" content="Чистка колодца, установка колодцев, копка колодцев, колодцы для воды, септики, установка септика, колодец для канализации" />
+
+        {/* Open Graph / Социальные сети */}
+        <meta property="og:title" content="Clear water | Чисты воды — Чистка, установка и ремонт колодцев" />
+        <meta property="og:description" content="Установка, обслуживание и ремонт колодцев/септиков." />
+        <meta property="og:image" content="https://clearwater.by/Img3.png" />
+        <meta property="og:url" content="https://clearwater.by" />
+        <meta property="og:type" content="website" />
+
+        {/* Favicon */}
+        <link rel="icon" href="/water-flash-fill.svg" />
+        <link rel="icon" href="/water-flash-fill.svg" type="image/svg" />
+        <link rel="icon" href="/water-flash-fill.svg" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="/water-flash-fill.svg" />
+
+        {/* Canonical URL */}
+        <link rel="canonical" href="https://clearwater.by" />
+
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "LocalBusiness",
+            "name": "Clearwater",
+            "address": {
+              "@type": "PostalAddress",
+              "addressLocality": "Витебск",
+              "addressCountry": "BY"
+            },
+            "telephone": "+375 (25) 669-15-81",
+            "url": "https://clearwater.by"
+           })}
+      </script> 
+      </Helmet>
+
     <module>
      <Contacts isOpenContacts = {isModalContactOpen} onCloseContacts = {()=> setModalContactOpen(false)}/>
      <Installation isOpenInstall = {isModalInstallOpen} onCloseInstall = {()=> setIsModalInstallOpen(false)}/>
